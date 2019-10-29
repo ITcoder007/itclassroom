ffe2360fc378464f563dfef63d85edc7ecf0eabe
gengjiaxing <gengjiaxing@192.168.3.6>
2019-10-29 11:29:00 +0800

    demo

--- src/main/java/com/xg/controller/HelloController.java
+++ src/main/java/com/xg/controller/HelloController.java
@@ -1,8 +1,13 @@
 package com.xg.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	@GetMapping("/")
+	public String hello() {
+		return "hello world!";
+	}
 }
